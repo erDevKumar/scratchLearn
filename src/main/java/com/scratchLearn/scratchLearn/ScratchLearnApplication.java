f9774a434ff48325cddc635b51389bc328a6e667
devender.kumar6 <devender.kumar6@olaelectric.com>
2024-06-04 17:15:21 +0530

    initial commit

---
+++ src/main/java/com/scratchLearn/scratchLearn/ScratchLearnApplication.java
@@ -0,0 +1,13 @@
+package com.scratchLearn.scratchLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScratchLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScratchLearnApplication.class, args);
+	}
+
+}
