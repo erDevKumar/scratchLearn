ee236bfce751ce1d4cf9929a7cac017029b3f0ef
Dev Kumar <erkumardevender@gmail.com>
2024-06-04 22:58:06 +0530

    jar is able to build now, app is working

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
-	kotlin("jvm")
+	kotlin("jvm") version "1.9.0"
 }
 
 group = "com.scratchLearn"
